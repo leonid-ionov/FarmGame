98eb9a787c947fb980a21ba88b85c2507cf116c9
Leonid Ionov <neonych@gmail.com>
2020-10-26 02:09:36 +0300

    Updates

---
+++ precache-manifest.3306437a1967f454fbea2c79318b164b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4059dce5c75d61eda3abd742ee57adf",
+    "url": "/FarmGame/index.html"
+  },
+  {
+    "revision": "08ddd6adf4169c47ac25",
+    "url": "/FarmGame/static/css/2.5f3a66ff.chunk.css"
+  },
+  {
+    "revision": "59c4b2d8e0c2af58de8d",
+    "url": "/FarmGame/static/css/main.b9be3418.chunk.css"
+  },
+  {
+    "revision": "08ddd6adf4169c47ac25",
+    "url": "/FarmGame/static/js/2.12d235d5.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/FarmGame/static/js/2.12d235d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c4b2d8e0c2af58de8d",
+    "url": "/FarmGame/static/js/main.c31b72c7.chunk.js"
+  },
+  {
+    "revision": "67eb342bff41e56311b5",
+    "url": "/FarmGame/static/js/runtime-main.b79219f1.js"
+  },
+  {
+    "revision": "24942eb3445fd067808efb6d895ff848",
+    "url": "/FarmGame/static/media/Chicken.24942eb3.png"
+  },
+  {
+    "revision": "729919509e2701ea3fa888f0be9a16c2",
+    "url": "/FarmGame/static/media/Cow.72991950.png"
+  },
+  {
+    "revision": "95e7e033285231afc0f5c68f173f71fc",
+    "url": "/FarmGame/static/media/Eggs.95e7e033.png"
+  },
+  {
+    "revision": "c554611d72d42f645d889d0eb5436e25",
+    "url": "/FarmGame/static/media/Grain.c554611d.png"
+  },
+  {
+    "revision": "1e0581a75853fd4c0ec381b1eacaf318",
+    "url": "/FarmGame/static/media/Grass.1e0581a7.jpg"
+  },
+  {
+    "revision": "d9166108a8180b4f886b37eeb0d61545",
+    "url": "/FarmGame/static/media/Logo.d9166108.png"
+  },
+  {
+    "revision": "915084a8ae610a2883a3281a5668cdf6",
+    "url": "/FarmGame/static/media/MainMenuImg.915084a8.jpg"
+  },
+  {
+    "revision": "cb9c039340ab865c5cfdc88529c3450f",
+    "url": "/FarmGame/static/media/Milk.cb9c0393.png"
+  },
+  {
+    "revision": "8523b238c1d8321a5b0b049e10a4131d",
+    "url": "/FarmGame/static/media/Money.8523b238.png"
+  },
+  {
+    "revision": "10ac25356d5e3de2ace8dcdb6ce97996",
+    "url": "/FarmGame/static/media/Shovel.10ac2535.png"
+  },
+  {
+    "revision": "3491c547d191292de4f1feb4d65cf08f",
+    "url": "/FarmGame/static/media/Wheat.3491c547.png"
+  }
+]);
